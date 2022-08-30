193df2fddd9f2e10b43a727506e2eaff404e0604
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-30 16:29:19 +0530

    added images

--- iac/010070-aks-vnet/commands.sh
+++ iac/010070-aks-vnet/commands.sh
@@ -84,30 +84,9 @@ az aks show --resource-group terraform-aks-dev --name terraform-aks-dev-aks-clus
 # terraform-aks-dev-aks-cluster-agentpool
 az identity list --resource-group terraform-aks-dev-nrg
 
+
 # Get pods
 kubectl get pods -n kube-system
-
-# Observe the output. You will see the aci-connector-linux-686f8748cc-7fld5 pod is not able to start.
-# See the image get pods. 2_GetPods.jpg
-# So look at the logs. Ensure you have the connect pod name below
-kubectl logs -f aci-connector-linux-57f5bbfc9-k4d5s -n kube-system
-
-# Observe closely the logs
-# The client 'cc7c275b-2f64-4718-9213-e6d4eb8fdc31' with object id 'cc7c275b-2f64-4718-9213-e6d4eb8fdc31' 
-# does not have authorization to perform action 'Microsoft.Network/virtualNetworks/subnets/read' over scope 
-# '/subscriptions/10588091-0196-44e3-a0b8-3h7e05259147/resourcegroups/terraform-aks-dev/providers/Microsoft.Network/virtualNetworks/aks-network/subnets/aks-default-subnet' 
-# or the scope is invalid. If access was recently granted, please refresh your credentials."
-
-# Now take a look at images 3_AciConnector1.jpg, 3_AciConnector2.jpg and Add Role Assignments
-
-# Now delete the aci-connector-linux pod
-kubectl delete pod aci-connector-linux-57f5bbfc9-d55g2 -n kube-system
-
-kubectl logs -f aci-connector-linux-57f5bbfc9-v6sjp -n kube-system
-
-# Observe the output. You will see the aci-connector-linux-686f8748cc-7fld5 pod is not able to start.
-# So look at the logs. Ensure you have the connect pod name below
-kubectl logs -f aci-connector-linux-54fb76ccd-m5xct -n kube-system
 
 # Now deploy the apps.
 
