a445ed3428312588b8a1f3d3cfb02b094f132108
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-31 01:30:24 +0530

    Edited ReadMe file

--- iac/010020-tf-aks/tf7-aks-cluster.tf
+++ iac/010020-tf-aks/tf7-aks-cluster.tf
@@ -29,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = "${azurerm_resource_group.aks_rg.name}-aks-cluster"
-  private_cluster_enabled = true
+  # private_cluster_enabled = true
   # dns_prefix_private_cluster = "${azurerm_resource_group.aks_rg.name}-aks-pvt-cluster"
   kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
@@ -85,7 +85,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   # }
 
   azure_policy_enabled = true
-  
+
   # The following  
   # aci_connector_linux {
   # subnet_name = ""
