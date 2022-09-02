73b0fd5534e3043edd9051181e834093942cc3e0
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-02 13:45:08 +0530

    added more info

--- iac/010140-kube-simple-yaml/commands.sh
+++ iac/010140-kube-simple-yaml/commands.sh
@@ -156,86 +156,10 @@ kubectl delete -f .\kube-manifests\4-service\
 
 ###################################################################################
 
-kubectl get deploy --namespace default
-
-kubectl create deployment my-backend-rest-app --image=stacksimplify/kube-helloworld:1.0.0 
-
-kubectl get deploy
-
-kubectl get rs
-
-kubectl get pod
-
-# Just verify, get the pod name.
-kubectl logs -f my-backend-rest-app-54fb6575d9-c6p64
-
-# Create ClusterIp Service for Backend Rest App
-# Note We don't need to specify "--type=ClusterIp" because default setting is to create ClusterIp Service.
-kubectl expose deployment my-backend-rest-app --port=8080 --target-port=8080 --name=my-backend-service
-kubectl get svc
-
-kubectl get svc -o wide
-
-kubectl get svc -o json
-
-# Install Jq(https://stedolan.github.io/jq/, download jq-win64.exe, rename that to jq.exe. Then place it in some folder, add that folder to the path env var)
-# The open a new command prompt and then exectte the following. It should give the type of the service as ClusterIP
-kubectl get svc my-backend-service -o json | jq '.spec.type'
-
-# Now the front end part.
-
-# Create Deployment for Frontend Nginx Proxy
-# kubectl create deployment my-frontend-nginx-app --image=stacksimplify/kube-frontend-nginx:1.0.0 
-kubectl create deployment my-frontend-nginx-app --image=avts/nginxvivekproxy:v1
-
-kubectl get deploy
-
-kubectl delete deploy my-frontend-nginx-app
-
-kubectl get pod
-
-# Create ClusterIp Service for Frontend Nginx Proxy
-kubectl expose deployment my-frontend-nginx-app  --type=LoadBalancer --port=80 --target-port=80 --name=my-frontend-service
-kubectl get svc
-
-kubectl get pod
-
-kubectl logs -f my-backend-rest-app-54fb6575d9-szwg9
-
-kubectl get svc my-frontend-service -o json
-
-# Capture External IP of the loadbalancer service.
-
-# Install Jq(https://stedolan.github.io/jq/, download jq-win64.exe, rename that to jq.exe. Then place it in some folder, add that folder to the path env var)
-# The open a new command prompt and then exectte the following. It should give the Extenal ip of the loadbalancer service
-kubectl get svc my-frontend-service -o json | jq '.status.loadBalancer.ingress[].ip'
-
-# Now browse to the ip address/hello.
-http://<EXTERNAL-IP>/hello
-
-# Scale backend with 10 replicas
-kubectl scale --replicas=10 deployment/my-backend-rest-app
-
-# Test again to view the backend service Load Balancing
-http://<EXTERNAL-IP>/hello
-
-##################################################################################################### 
-
-# Clean up
-
-# Delete Deployment
-kubectl delete deployment my-backend-rest-app
-kubectl delete deployment my-frontend-nginx-app
-
-# Delete Service
-kubectl delete svc my-backend-service
-
-kubectl delete svc my-frontend-service
-
 # Get all Objects from Kubernetes default namespace
 kubectl get all
 
-##############################################################################
+###################################################################################
 
 terraform plan -destroy -out main.destroy.tfplan
 
